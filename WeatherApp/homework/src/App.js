a315a50881be82ba10dd97e2433dfbb41d81ef70
lucasfer01 <85971126+lucasfer01@users.noreply.github.com>
2021-10-21 21:33:53 -0300

    creacion

--- WeatherApp/homework/src/App.js
+++ WeatherApp/homework/src/App.js
@@ -5,10 +5,14 @@ import Card from './components/Card.jsx';
 import Cards from './components/Cards.jsx';
 import SearchBar from './components/SearchBar.jsx';
 import data, { Cairns } from './data.js';
+import Nav from './components/Nav';
+import img from './img/henry-icon.png';
+
 
 function App() {
   return (
     <div className='App'>
+      <Nav title='Weather App' src={img} />
       <div className={stilos.contenedorPrincipal}>
         <Card
           max={Cairns.main.temp_max}
