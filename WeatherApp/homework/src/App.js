5b92bb283751f9a88bb3642725ec5ca03b639569
lucasfer01 <85971126+lucasfer01@users.noreply.github.com>
2021-10-22 21:14:49 -0300

    cambios

--- WeatherApp/homework/src/App.js
+++ WeatherApp/homework/src/App.js
@@ -1,17 +1,16 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
-import stilos from './styles/card.module.css';
-import Card from './components/Card.jsx';
+// import stilos from './styles/card.module.css';
+// import Card from './components/Card.jsx';
 import Cards from './components/Cards.jsx';
-import SearchBar from './components/SearchBar.jsx';
-import data, { Cairns } from './data.js';
+// import SearchBar from './components/SearchBar.jsx';
+// import { Cairns } from './data.js';
 import Nav from './components/Nav';
 import img from './img/henry-icon.png';
 
+const API_KEY = process.env.REACT_APP_API_KEY;
 
 function App() {
-<<<<<<< Updated upstream
-=======
   const [cities, setCities] = useState([]);
 
   function handleAddCity(city) {
@@ -55,31 +54,10 @@ function App() {
       });
   }
 
->>>>>>> Stashed changes
   return (
-    <div className='App'>
-      <Nav title='Weather App' src={img} />
-      <div className={stilos.contenedorPrincipal}>
-        <Card
-          max={Cairns.main.temp_max}
-          min={Cairns.main.temp_min}
-          name={Cairns.name}
-          img={Cairns.weather[0].icon}
-          onClose={() => alert(Cairns.name)}
-        />
-      </div>
-      <hr />
-      <div>
-        <Cards
-          cities={data}
-        />
-      </div>
-      <hr />
-      <div>
-        <SearchBar
-          onSearch={(ciudad) => alert(ciudad)}
-        />
-      </div>
+    <div className="App">
+      <Nav title='Weather App' src={img} onSearch={onSearch} />
+      <Cards cities={cities} onRemove={handleRemoveCity} />
     </div>
   );
 }
--- WeatherApp/homework/src/components/Nav.jsx
+++ WeatherApp/homework/src/components/Nav.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import SearchBar from './SearchBar'
-function Nav({title, src}) {
+function Nav({title, src, onSearch}) {
     return (
         <div style={{margin: 'auto', display:'flex', justifyContent: 'space-around', alignItems: 'center', width: '100%', height: '5em', backgroundColor: '#023e8a', boxShadow: '0 0 6px rgba(0,0,0,0.5)'}}>
             <img style={{width: '40px', height: '40px'}} src={src} alt="" />
             <h2>{title}</h2>
-            <SearchBar  onSearch={(ciudad) => alert(ciudad)} />
+            <SearchBar  onSearch={onSearch} />
         </div>
     )
 }
